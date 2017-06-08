01198d608ab8d5dc8477da5df8e1cdafb2985c6a
Aaron Philip <aaron.philip@medlever.com>
2017-06-08 14:35:31 -0700

    some new styles for the profileList and added a css reset

--- Ng/user.ts
+++ Ng/user.ts
@@ -1,12 +1,16 @@
 export class User {
 	id: number;
+	title: string; //job title or "student"
 	firstName: string;
 	lastName: string;
 	occupation: string;
-	major: string;
-	college: string;
 	degree: string;
+	major: string;
 	specialization: string;
+	college: string;
 	email: string;
 	phone: number;
+	city: string;
+	state: string;
+	classification: string; //student, alumni, family or friend
 }
